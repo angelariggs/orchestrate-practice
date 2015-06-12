0b69980d8a1637023bd6d2b095478ded86dc637a
[angela riggs] <[riggs.ang@gmail.com]>
2015-06-12 10:13:57 -0700

    added comments to index

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 var config = require ('./config.js')
 var db = require('orchestrate')(config.dbKey)
 
+/*Puts the prop/values color, hometown, and school into the jsi-orchestrate collection*/
+// db.put('jsi-orchestrate', 'angela', {
+//   "color": "blue",
+//   "hometown":"Melbourne",
+//   "school":"Portland Code School"
+// })
+// .then(function(res) {
+//   console.log(res.statusCode);
+// });
 
-db.put('jsi-orchestrate', 'angela', {
-  "color": "blue",
-  "hometown":"Melbourne",
-  "school":"Portland Code School"
-})
-.then(function(res) {
-  console.log(res.statusCode);
-});
+/*Added prop/val car to the existing collection*/
+// db.newPatchBuilder('jsi-orchestrate', 'angela')
+//   .add('car', 'Jetta')
+//   .apply()
+//   .then(function(result) {
+//     console.log()
+//   });
 
-db.newPatchBuilder('jsi-orchestrate', 'angela')
-  .add('car', 'Jetta')
-  .apply()
-  .then(function(result) {
-    console.log()
-  });
-
-db.put('jsi-orchestrate', 'angela', {
-  'partner':'Josh'
-})
-.then(function() {
-  db.get('jsi-orchestrate', 'angela')
-  .then(function(res) {
-    console.log(res.body);
-  })
-});
+/*Used in the same way as patchbuilder.*/
+// db.put('jsi-orchestrate', 'angela', {
+//   'partner':'Josh'
+// })
+// .then(function() {
+//   db.get('jsi-orchestrate', 'angela')
+//   .then(function(res) {
+//     console.log(res.body);
+//   })
+// });
 
 
   
